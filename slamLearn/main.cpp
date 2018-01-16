6aa62bd8a6787b29aeac271e164d424edbc6124e
“mjwz5294” <393578079@qq.com>
2018-01-16 20:54:26 +0800

    hello slam

---
+++ slamLearn/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  slamLearn
+//
+//  Created by mac on 2018/1/16.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, SLAM!\n" << endl;
+    return 0;
+}
